65a8e7beb99b6c24cae0dc30e9f7cd7f7dc47970
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2022-10-07 10:49:20 +0000

    added record for death and brith, insertion successfully

--- dashboard.php
+++ dashboard.php
@@ -1,55 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head> 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!-- Primary Meta Tags -->
-<title>Volt - Free Bootstrap 5 Admin Dashboard</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="title" content="Volt - Free Bootstrap 5 Admin Dashboard">
-<meta name="author" content="Themesberg">
-<meta name="description" content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
-<meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, themesberg, themesberg dashboard, themesberg admin dashboard" />
-<link rel="canonical" href="https://themesberg.com/product/admin-dashboard/volt-premium-bootstrap-5-dashboard">
-
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://demo.themesberg.com/volt-pro">
-<meta property="og:title" content="Volt - Free Bootstrap 5 Admin Dashboard">
-<meta property="og:description" content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
-<meta property="og:image" content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/volt-pro-bootstrap-5-dashboard/volt-pro-preview.jpg">
-
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://demo.themesberg.com/volt-pro">
-<meta property="twitter:title" content="Volt - Free Bootstrap 5 Admin Dashboard">
-<meta property="twitter:description" content="Volt Pro is a Premium Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
-<meta property="twitter:image" content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/volt-pro-bootstrap-5-dashboard/volt-pro-preview.jpg">
-
-<!-- Favicon -->
-<link rel="apple-touch-icon" sizes="120x120" href="./assets/img/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/favicon-16x16.png">
-<link rel="manifest" href="./assets/img/favicon/site.webmanifest">
-<link rel="mask-icon" href="./assets/img/favicon/safari-pinned-tab.svg" color="#ffffff">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="theme-color" content="#ffffff">
-
-<!-- Sweet Alert -->
-<link type="text/css" href="./vendor/sweetalert2/dist/sweetalert2.min.css" rel="stylesheet">
-
-<!-- Notyf -->
-<link type="text/css" href="./vendor/notyf/notyf.min.css" rel="stylesheet">
-
-<!-- Volt CSS -->
-<link type="text/css" href="./css/volt.css" rel="stylesheet">
-
-<!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
-
-</head>
-
-<body>
+<?php require_once("./includes/dashboard-head.php");?>
 
        <?php
        
@@ -57,9 +6,6 @@
        
        ;?>
         
-
-      
-    
         <main class="content">
 
             <?php require_once("./includes/dashboard-top-nav-bar.php");?>
@@ -69,74 +15,4 @@
                   <?php require_once("./includes/dashboard-page-visit.php");?>
                        <?php require_once("./includes/dashboard-team.php");?>
                        
-
-<footer class="bg-white rounded shadow p-5 mb-4 mt-4">
-    <div class="row">
-        <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
-            <p class="mb-0 text-center text-lg-start">© 2019-<span class="current-year"></span> <a class="text-primary fw-normal" href="https://themesberg.com" target="_blank">Themesberg</a></p>
-        </div>
-        <div class="col-12 col-md-8 col-xl-6 text-center text-lg-start">
-            <!-- List -->
-            <ul class="list-inline list-group-flush list-group-borderless text-md-end mb-0">
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/about">About</a>
-                </li>
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/themes">Themes</a>
-                </li>
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/blog">Blog</a>
-                </li>
-                <li class="list-inline-item px-0 px-sm-2">
-                    <a href="https://themesberg.com/contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</footer>
-        </main>
-
-        <!-- Core -->
-<script src="./vendor/@popperjs/core/dist/umd/popper.min.js"></script>
-<script src="./vendor/bootstrap/dist/js/bootstrap.min.js"></script>
-
-<!-- Vendor JS -->
-<script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
-
-<!-- Slider -->
-<script src="./vendor/nouislider/distribute/nouislider.min.js"></script>
-
-<!-- Smooth scroll -->
-<script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
-
-<!-- Charts -->
-<script src="./vendor/chartist/dist/chartist.min.js"></script>
-<script src="./vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
-
-<!-- Datepicker -->
-<script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
-
-<!-- Sweet Alerts 2 -->
-<script src="./vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
-
-<!-- Moment JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
-
-<!-- Vanilla JS Datepicker -->
-<script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
-
-<!-- Notyf -->
-<script src="./vendor/notyf/notyf.min.js"></script>
-
-<!-- Simplebar -->
-<script src="./vendor/simplebar/dist/simplebar.min.js"></script>
-
-<!-- Github buttons -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-
-<!-- Volt JS -->
-<script src="./assets/js/volt.js"></script>
-
-</body>
-
-</html>
+                <?php require_once("./includes/dashboard-footer.php");?>
--- includes/dashboard-side-bar.php
+++ includes/dashboard-side-bar.php
@@ -29,13 +29,13 @@
         <a href="#sidebarMenu" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true"
             aria-label="Toggle navigation">
-            <svg class="icon icon-xs" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <svg class="icon icon-xs" fill="currentColor" viewBox="0 0 20 20" xmlns=""><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </a>
       </div>
     </div>
     <ul class="nav flex-column pt-3 pt-md-0">
       <li class="nav-item">
-        <a href="../../index.html" class="nav-link d-flex align-items-center">
+        <a href="././index.php" class="nav-link d-flex align-items-center">
           <span class="sidebar-icon">
             <!-- <img src="./assets/img/brand/light.svg" height="20" width="20" alt="Volt Logo"> -->
           </span>
@@ -43,7 +43,7 @@
         </a>
       </li>
       <li class="nav-item  active ">
-        <a href="../../pages/dashboard/dashboard.html" class="nav-link">
+        <a href="././index.php" class="nav-link">
           <span class="sidebar-icon">
             <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
           </span> 
